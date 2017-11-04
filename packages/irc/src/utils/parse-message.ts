5795abce8ba3c2c0a061e1e28902be9ec534b0c5
f0und3r <f0und3r@inbox.ru>
2017-11-04 12:38:33 +0300

    parseMessage отрабатывает отлично

--- packages/irc/src/utils/parse-message.ts
+++ packages/irc/src/utils/parse-message.ts
@@ -1,11 +1,54 @@
 import { Message } from "../types"
 
-const cardRegExp = /^#main : \[Server\].*'(.+)'.*'(.+)'/
-const refineRegExp = /^#main : \[Server\].*'(.+)'.*\+(\d+) (.+)./
+const cardRegExp = /^#main : \[Server\].*'(.+)'.*'(.+)'\. Грац!/
+const refineRegExp = /^#main : \[Server\].*'(.+)'.*заточил.*\+(\d+) (.+)./
 const buyRegExp = /^#main : \[Server\].*'(.+)'.*скупку.*'(.+)'.*: (.+) <(\d+),(\d+)>/
 const sellRegExp = /^#main : \[Server\].*'(.+)'.*'(.+)'.*: (.+) <(\d+),(\d+)>/
 
 export default (mes: string): Message => {
+  const cardMatches = mes.match(cardRegExp)
+  if (cardMatches) {
+    return {
+      type: "card",
+      owner: cardMatches[1],
+      item: cardMatches[2]
+    }
+  }
+
+  const refineMatches = mes.match(refineRegExp)
+  if (refineMatches) {
+    return {
+      type: "refine",
+      owner: refineMatches[1],
+      refine: parseInt(refineMatches[2]) || 0,
+      item: refineMatches[3]
+    }
+  }
+
+  const buyMatches = mes.match(buyRegExp)
+  if (buyMatches) {
+    return {
+      type: "buy",
+      owner: buyMatches[1],
+      title: buyMatches[2],
+      map: buyMatches[3],
+      x: parseInt(buyMatches[4]) || 0,
+      y: parseInt(buyMatches[5]) || 0
+    }
+  }
+
+  const sellMatches = mes.match(sellRegExp)
+  if (sellMatches) {
+    return {
+      type: "sell",
+      owner: sellMatches[1],
+      title: sellMatches[2],
+      map: sellMatches[3],
+      x: parseInt(sellMatches[4]) || 0,
+      y: parseInt(sellMatches[5]) || 0
+    }
+  }
+
   return {
     type: "text",
     text: mes
--- packages/irc/tests/parse-message.ts
+++ packages/irc/tests/parse-message.ts
@@ -1,3 +1,65 @@
+import { expect } from "chai"
 import parseMessage from "../src/utils/parse-message"
+import {
+  TextMessage,
+  CardMessage,
+  RefineMessage,
+  BuyMessage,
+  SellMessage
+} from "../src/types"
 
-describe("parseMessage", () => {})
+describe("parseMessage", () => {
+  it("should return TextMessage", () => {
+    const text = "Это обычное текстовое сообщение"
+    const message = parseMessage(text) as TextMessage
+
+    expect(message.type).to.equal("text")
+    expect(message.text).to.equal("Это обычное текстовое сообщение")
+  })
+
+  it("should return CardMessage", () => {
+    const text = "#main : [Server] 'Анеша' выбила 'Cursering Card'. Грац!"
+    const message = parseMessage(text) as CardMessage
+
+    expect(message.type).to.equal("card")
+    expect(message.owner).to.equal("Анеша")
+    expect(message.item).to.equal("Cursering Card")
+  })
+
+  it("should return RefineMessage", () => {
+    const text =
+      "#main : [Server] 'Mortesadi' успешно заточил +5 Orc Archer Bow[0]."
+    const message = parseMessage(text) as RefineMessage
+
+    expect(message.type).to.equal("refine")
+    expect(message.owner).to.equal("Mortesadi")
+    expect(message.refine).to.equal(5)
+    expect(message.item).to.equal("Orc Archer Bow[0]")
+  })
+
+  it("should return BuyMessage", () => {
+    const text =
+      "#main : [Server] 'ОпятьРабота' открывает скупку под названием 'СиняяТрава 1600' (коорд.: payon <160,57>)."
+    const message = parseMessage(text) as BuyMessage
+
+    expect(message.type).to.equal("buy")
+    expect(message.owner).to.equal("ОпятьРабота")
+    expect(message.title).to.equal("СиняяТрава 1600")
+    expect(message.map).to.equal("payon")
+    expect(message.x).to.equal(160)
+    expect(message.y).to.equal(57)
+  })
+
+  it("should return SellMessage", () => {
+    const text =
+      "#main : [Server] 'SellAll' открывает магазин 'Желтые гемы' (коорд.: izlude <138,128>)."
+    const message = parseMessage(text) as SellMessage
+
+    expect(message.type).to.equal("sell")
+    expect(message.owner).to.equal("SellAll")
+    expect(message.title).to.equal("Желтые гемы")
+    expect(message.map).to.equal("izlude")
+    expect(message.x).to.equal(138)
+    expect(message.y).to.equal(128)
+  })
+})
